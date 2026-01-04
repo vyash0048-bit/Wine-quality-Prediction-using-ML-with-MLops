21d07124683bb80a0ee96f84ae32dcba4640d6df
vyash0048 <vyash0048@gmail.com>
2026-01-04 15:43:52 +0530

    Model evaluation and MLflow added

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+import os
+import mlflow
+import dagshub
+
+# ---------- MLflow + DagsHub SETUP (ONCE) ----------
+os.environ["MLFLOW_TRACKING_USERNAME"] = "vyash0048"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "ff0a8bd84e9708a0fc9bd7259a0b2fc85fcec80e"
+
+mlflow.set_tracking_uri(
+    "https://dagshub.com/vyash0048/MLproject-with-MLflow.mlflow"
+)
+
+dagshub.init(
+    repo_owner="vyash0048",
+    repo_name="MLproject-with-MLflow",
+    mlflow=True
+)
+# --------------------------------------------------
+
+
+
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -46,3 +68,12 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -3,7 +3,8 @@ from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
-                                            ModelTrainerConfig)
+                                            ModelTrainerConfig,
+                                            ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -88,7 +89,7 @@ class ConfigurationManager:
     
 
 
-"""    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         config = self.config.model_evaluation
         params = self.params.ElasticNet
         schema =  self.schema.TARGET_COLUMN
@@ -102,9 +103,9 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow",
+            mlflow_uri="https://dagshub.com/vyash0048/MLproject-with-MLflow.mlflow",
            
         )
 
         return model_evaluation_config
-"""
+    
